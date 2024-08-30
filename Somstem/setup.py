244ac05c1e566e03caab74c935b86ad58fee1d11
Abdisalamm <58686799+Abdisalamm@users.noreply.github.com>
2024-08-30 13:09:46 +0100

    Add files via upload

---
+++ Somstem/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Somali',
+    version='1.0',
+    author='Abdisalam B.',
+    author_email='fiicane121@gmail.com',
+    description='This is a rule based stemmer for somali language.',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
